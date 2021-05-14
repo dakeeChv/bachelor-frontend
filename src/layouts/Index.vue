3a0ebe5b411bc7ce684c961aee93cd9859b51dd1
Daky <dkartree@gmail.com>
2021-05-14 16:07:48 +0700

    first commit

---
+++ src/layouts/Index.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list>
+        <v-subheader>Dashbaord</v-subheader>
+        <v-list-item-group v-model="selectedItem" color="deep-purple accent-4">
+          <v-list-item v-for="(item, i) in items" :key="i">
+            <v-list-item-icon>
+              <v-icon v-text="item.icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.text"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar app fixed color="deep-purple accent-4" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Title</v-toolbar-title>
+    </v-app-bar>
+    <v-main>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+    <v-footer app absolute inset dark padless>
+      <v-card flat tile class="deep-purple accent-4 white--text text-center">
+        <v-card-text>
+          <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            class="mx-4 white--text"
+            icon
+          >
+            <v-icon size="24px">
+              {{ icon }}
+            </v-icon>
+          </v-btn>
+        </v-card-text>
+
+        <v-card-text class="white--text pt-0">
+          Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet.
+          Mauris cursus commodo interdum. Praesent ut risus eget metus luctus
+          accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim
+          a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula
+          lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus
+          iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum
+          tempor vel ut orci. Orci varius natoque penatibus et magnis dis
+          parturient montes, nascetur ridiculus mus.
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    icons: ['mdi-facebook', 'mdi-twitter', 'mdi-linkedin', 'mdi-instagram'],
+    selectedItem: 1,
+    drawer: false,
+    items: [
+      { text: 'Real-Time', icon: 'mdi-clock' },
+      { text: 'Audience', icon: 'mdi-account' },
+      { text: 'Conversions', icon: 'mdi-flag' }
+    ]
+  })
+}
+</script>
---
+++ src/views/auth/Login.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col class="col-md-6 col-sm-12 mx-auto mt-16">
+        <v-card flat>
+          <v-toolbar color="red darken-3" dark dense flat>
+            <v-toolbar-title>Login</v-toolbar-title>
+          </v-toolbar>
+          <v-form class="my-4 pa-6">
+            <v-text-field
+              type="text"
+              color="red darken-2"
+              label="Username Or Email Address"
+              placeholder="Username or Email"
+              prepend-icon="mdi-account"
+              hint="We'll never share your username or email with anyone else."
+              persistent-hint
+              light
+              outlined
+            ></v-text-field>
+            <v-text-field
+              class="mt-2"
+              type="password"
+              color="red darken-2"
+              label="Password"
+              placeholder="Password"
+              prepend-icon="mdi-lock"
+              light
+              outlined
+            ></v-text-field>
+            <div class="text-center">
+              <v-btn color="red accent-4" class="px-8 py-4 white--text">
+                Login
+              </v-btn>
+            </div>
+          </v-form>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
