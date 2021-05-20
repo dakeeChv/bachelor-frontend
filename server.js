900c811955ba629e415511ca1aa3a7ac78de97fb
Daky <dkartree@gmail.com>
2021-05-20 17:48:13 +0700

    try to heroku...

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const path = require('path')
 
 const app = express()
 
-app.use(serveStatic(path.join(__dirname, 'public-optimized')))
-app.use(serveStatic(path.join(__dirname, 'public')))
-app.listen(3000)
+//here we are configuring dist to serve app files
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function (req, res) {
+    res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
+const port = process.env.PORT || 8080
+app.listen(port)
+console.log(`app is listening on port: ${port}`)
